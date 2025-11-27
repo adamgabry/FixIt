d880607f83254a4d9b054b2d65642227f3bd27d2
Silvia Bieliková <sisa@bielik.sk>
2025-11-27 22:45:24 +0100

    chore: set up linter with prettier and clean up existing files

--- src/modules/user/schema.ts
+++ src/modules/user/schema.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 
 export const userSchema = z.object({
-    id: z.number(),
-    name: z.string(),
-    email: z.string().email().min(1),
-    password: z.string().min(1),
-    role: z.array(z.enum(['ADMIN', 'STUFF', 'USER'])),
+	id: z.number(),
+	name: z.string(),
+	email: z.string().email().min(1),
+	password: z.string().min(1),
+	role: z.array(z.enum(['ADMIN', 'STUFF', 'USER']))
 });
 
 export type User = z.infer<typeof userSchema>;
