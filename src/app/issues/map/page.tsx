cdc5b6f8a115095c6924b50717df1a4e5ec3edb0
Tomáš Ebert <xebert@fi.muni.cz>
2025-11-29 16:10:38 +0100

    feat: Add map to the issue page

--- src/app/issues/map/page.tsx
+++ src/app/issues/map/page.tsx
@@ -1,3 +1,18 @@
-const IssuesMapPage = () => 'Issues - map view';
+"use client";
+import dynamic from "next/dynamic";
+
+const MapComponent = dynamic(() => import("@/components/map"), {
+  ssr: false,
+});
+
+const IssuesMapPage = () => {
+	return <MapComponent 
+        center={[48.1486, 17.1077]} 
+        zoom={20} 
+        style={{ height: 500 }} 
+        initialMarkers={[]} 
+        canCreateMarker={false} 
+    />;
+};
 
 export default IssuesMapPage;
