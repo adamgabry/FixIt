06424951616853f5312b5624f11b4aaed108a82b
Tomáš Ebert <xebert@fi.muni.cz>
2025-11-30 14:32:51 +0100

    feat: Delete button

---
+++ src/components/delete-button.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Trash2 } from 'lucide-react';
+import { Button, type ButtonProps } from './button';
+
+export type DeleteButtonProps = Omit<ButtonProps, 'variant'> & {
+	showIcon?: boolean;
+};
+
+const DeleteButton = React.forwardRef<HTMLButtonElement, DeleteButtonProps>(
+	({ children, showIcon = true, ...props }, ref) => {
+		return (
+			<Button variant="destructive" ref={ref} {...props}>
+				{showIcon && <Trash2 />}
+				{children}
+			</Button>
+		);
+	}
+);
+DeleteButton.displayName = 'DeleteButton';
+
+export { DeleteButton };
+
