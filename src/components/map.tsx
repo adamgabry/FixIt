00bfa7e56a6313ab6736a7bd8cc4c3d2bb776bc3
Tomáš Ebert <xebert@fi.muni.cz>
2025-11-30 16:28:23 +0100

    fix: Map was used by other instance

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,17 +1,19 @@
 "use client";
-import { useState} from "react";
+import { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import type { LatLng } from "leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 // Fix for default marker icon in Next.js
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-	iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-	iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-	shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-});
+if (typeof window !== "undefined") {
+	delete (L.Icon.Default.prototype as any)._getIconUrl;
+	L.Icon.Default.mergeOptions({
+		iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+		iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+		shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+	});
+}
 
 type MapComponentProps = {
 	center: [number, number];
@@ -33,13 +35,24 @@ function ClickMarker({ onAdd }: { onAdd: (latlng: LatLng) => void }) {
 const MapComponent = ({ center, zoom, style, canCreateMarker = true, initialMarkers = [] }: MapComponentProps) => {
   const [markers, setMarkers] = useState<LatLng[]>(initialMarkers);
 
+  useEffect(() => {
+    if (initialMarkers.length > 0) {
+      setMarkers(initialMarkers);
+    }
+  }, [initialMarkers]);
+
   const addMarker = (latlng: LatLng) => {
     setMarkers(prev => [...prev, latlng]);
     // You can also send it to your backend here
   };
 
   return (
-    <MapContainer center={center} zoom={zoom} style={style}>
+    <MapContainer 
+      key={`map-${center[0]}-${center[1]}`}
+      center={center} 
+      zoom={zoom} 
+      style={style}
+    >
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
