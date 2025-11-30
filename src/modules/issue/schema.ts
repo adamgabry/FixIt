03fe73fb5565f31c68a045ff90634186024ea542
Tomáš Ebert <xebert@fi.muni.cz>
2025-11-30 20:19:51 +0100

    feat: Create enums

--- src/modules/issue/schema.ts
+++ src/modules/issue/schema.ts
@@ -1,12 +1,24 @@
 import { z } from 'zod';
 
+export enum IssueStatus {
+	OPEN = 'OPEN',
+	IN_PROGRESS = 'IN_PROGRESS',
+	CLOSED = 'CLOSED'
+}
+
+export enum IssueType {
+	BUG = 'BUG',
+	FEATURE = 'FEATURE',
+	TASK = 'TASK'
+}
+
 export const issueSchema = z.object({
 	id: z.number(),
 	title: z.string().min(1),
 	description: z.string().min(1),
 	location: z.string().min(1),
-	status: z.enum(['OPEN', 'IN_PROGRESS', 'CLOSED']),
-	type: z.enum(['BUG', 'FEATURE', 'TASK']),
+	status: IssueStatus,
+	type: IssueType,
 	pictures: z.string().min(1),
 	createdAt: z.date(),
 	upddatedAt: z.date(),
